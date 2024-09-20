e2f6902cbd9b7b51f97cca2cd5c991c774d47344
Gagan <142842464+gagan-kotian@users.noreply.github.com>
2024-09-20 23:33:16 +0530

    Update May.sol

--- contracts/May.sol
+++ contracts/May.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract CrowdFunding {  
+contract ElectoralBond {  
     struct Campaign {  
         address owner;
         string title;
